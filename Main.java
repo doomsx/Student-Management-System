c9921fa3c29e6337a13653fd2d7901b4483b19b9
Dominique <dlmagat08@gmail.com>
2025-06-21 13:22:09 +0800

    scanner

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("Student Management System");
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        String choose;
+
+        do{
+            System.out.println("Yes or No");
+            choose = scanner.nextLine();
+            count++;
+            System.out.println(count);
+        } while (choose.equalsIgnoreCase("yes") );
+
+        scanner.close();
     }
 }
