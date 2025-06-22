d51e5c467dfe4808a53ffd9709cccc254b0f5003
Dominique <dlmagat08@gmail.com>
2025-06-22 21:11:34 +0800

    change to try resources

--- Main.java
+++ Main.java
@@ -3,44 +3,48 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         System.out.println("Student Management System");
-        Scanner scanner = new Scanner(System.in);
-        int choice;
 
-        do{
-            System.out.println("\n---Student Management System---");
-            System.out.println("1. Add Student");
-            System.out.println("2. View All Student");
-            System.out.println("3. Search by Student ID");
-            System.out.println("4. Update Student");
-            System.out.println("5. Delete Student");
-            System.out.println("6. Exit");
-            System.out.print("Choose an option: ");
-            choice = scanner.nextInt();
+        try(Scanner scanner = new Scanner(System.in)){
 
-            switch (choice) {
-                case 1:
+            int choice;
+
+            do{
+                System.out.println("___________________________________");
+                System.out.println("\n---Student Management System---");
+                System.out.println("1. Add Student");
+                System.out.println("2. View All Student");
+                System.out.println("3. Search by Student ID");
+                System.out.println("4. Update Student");
+                System.out.println("5. Delete Student");
+                System.out.println("6. Exit");
+                System.out.print("Choose an option: ");
+                choice = scanner.nextInt();
+                System.out.println("___________________________________");
+
+                switch (choice) {
+                    case 1:
                     System.out.println("Chosen option 1");
                     break;
-                case 2:
+                    case 2:
                     System.out.println("Chosen option 2");
                     break;
-                case 3:
+                    case 3:
                     System.out.println("Chosen option 3");
                     break;
-                case 4:
+                    case 4:
                     System.out.println("Chosen option 4");
                     break;
-                case 5:
+                    case 5:
                     System.out.println("Chosen option 5");
                     break;
-                case 6:
+                    case 6:
                     System.out.println("Exit");
                     break;
-                default: 
+                    default: 
                     System.out.println("Invalid Option");
-            }
-        } while (choice != 6 );
+                }
+            } while (choice != 6 );
 
-        scanner.close();
+        }
     }
 }
